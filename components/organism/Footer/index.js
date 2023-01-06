cb969001da12fb23c172d1ccc07fad4d4141991f
LeonardoTononi <easaaa@protonmail.com>
2023-01-06 12:59:35 +0100

    udpate footer

--- components/organism/Footer/index.js
+++ components/organism/Footer/index.js
@@ -1,16 +1,17 @@
-import useLocale from 'hooks/useLocale';
-import Link from 'next/link';
-import { SiInstagram, SiFacebook } from 'react-icons/si';
-import Image from 'next/image';
+import useLocale from "hooks/useLocale";
+import Link from "next/link";
+import { SiInstagram, SiFacebook } from "react-icons/si";
+import Image from "next/image";
 export default function Footer({
   t: {
     navbar: { home, lodge, apartments, activities, contact },
   },
 }) {
   const { locale } = useLocale();
+  let year = new Date().getFullYear();
   return (
-    <footer className='relative z-50 flex flex-col justify-end w-full py-10 text-gray-800 bg-bottom bg-gray-50 max-h-800'>
-      <div className='z-50 flex justify-center w-full pb-4 md:pb-10'>
+    <footer className="relative z-50 flex flex-col justify-end w-full pt-10 text-gray-800 bg-bottom bg-gray-50 max-h-800">
+      <div className="z-50 flex justify-center w-full pb-4 md:pb-10">
         {/*  <img
           src={'/black-logo.png'}
           className={`object-container object-center`}
@@ -22,63 +23,77 @@ export default function Footer({
         /> */}
       </div>
 
-      <ul className='z-50 grid max-w-screen-sm gap-3 pb-16 mx-auto text-center place-content-center md:grid-cols-4'>
-        <li className='font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl'>
+      <ul className="z-50 grid max-w-screen-sm gap-3 pb-16 mx-auto text-center place-content-center md:grid-cols-4">
+        <li className="font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl">
           <Link href={`/${locale}`}>
             <a>{home}</a>
           </Link>
         </li>
-        <li className='font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl'>
+        <li className="font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl">
           <Link href={`/${locale}/agriturismo`}>
             <a>{lodge}</a>
           </Link>
         </li>
-        <li className='font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl'>
+        <li className="font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl">
           <Link href={`/${locale}/appartamenti`}>
             <a>{apartments}</a>
           </Link>
         </li>
-        <li className='font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl'>
+        <li className="font-sans text-lg font-light tracking-wider text-gray-800 cursor-pointer lg:hover:border-white md:text-xl">
           <Link href={`/${locale}/attivita`}>
             <a>{activities}</a>
           </Link>
         </li>
       </ul>
 
-      <div className='z-50 grid font-light text-center place-content-center'>
-        <span className='flex justify-center pb-16'>
+      <div className="z-50 grid font-light text-center place-content-center">
+        <span className="flex justify-center pb-16">
           <a
-            href='https://www.facebook.com/H.Vittoria'
-            target='_blank'
-            className='text-gray-800 transition transform hover:scale-110'>
-            <SiFacebook className='text-2xl' />
+            href="https://www.facebook.com/H.Vittoria"
+            target="_blank"
+            className="text-gray-800 transition transform hover:scale-110"
+          >
+            <SiFacebook className="text-2xl" />
           </a>
 
           <a
-            href='https://www.instagram.com/hotelvittoria.lagodigarda/'
-            target='_blank'
-            className='ml-4 text-gray-800 transition transform hover:scale-110'>
-            <SiInstagram className='text-2xl' />
+            href="https://www.instagram.com/hotelvittoria.lagodigarda/"
+            target="_blank"
+            className="ml-4 text-gray-800 transition transform hover:scale-110"
+          >
+            <SiInstagram className="text-2xl" />
           </a>
         </span>
-        <p className='leading-normal text-gray-800'>
+        <p className="leading-normal text-gray-800">
           Via Benamati 114
           <br />
           Toscolano Maderno (BS) - 25088
         </p>
-        <div className='grid gap-2 mt-6 mb-8 text-gray-800 underline'>
-          <a href='mailto:info@sanlorenzodipersegno.it'>
+        <div className="grid gap-2 mt-6 mb-8 text-gray-800 underline">
+          <a href="mailto:info@sanlorenzodipersegno.it">
             info@vittoriahotel.eu
           </a>
-          <a href='tel:00393293103384'>Tel. +39 0365 64 1320</a>
+          <a href="tel:00393293103384">Tel. +39 0365 64 1320</a>
+        </div>
+      </div>
+
+      <div className="grid font-light text-center place-content-center mt-10 bg-black text-gray-100 pb-6">
+        <div className="grid mt-6 mb-8  text-sm">
+          <p className="leading-normal ">P.iva 01558810980</p>
+          <p className="leading-normal ">Codice Regione 017187-ALB-00011</p>
+          <p className="leading-normal ">
+            Ragione sociale - Hotel Vittoria
+          </p>
+          <p className="leading-normal ">Tononi Luca Antonio</p>
         </div>
 
         <a
-          className='text-gray-800'
-          href='https://leonardotononi.com'
-          target='_blank'
-          title='Web Developer Website'>
-          © 2021 powered by Leonardo Tononi
+          className="underline text-sm"
+          href="https://leonardotononi.com"
+          target="_blank"
+          title="Web Developer Website"
+        >
+          © {year} powered by Leonardo Tononi
         </a>
       </div>
     </footer>
